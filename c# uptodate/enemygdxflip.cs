eb19b5ca2a34ab56a1379c4ba880df1fba1bd836
iron5050 <43746689+iron5050@users.noreply.github.com>
2021-05-14 13:38:12 +0000

    Add files via upload

---
+++ c# uptodate/enemygdxflip.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class enemygdxflip : MonoBehaviour
+{
+    public AIPath aiPath;
+
+
+
+    // snýr spritinu við miða við movement
+    void Update()
+    {
+        if(aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        } else if (aiPath.desiredVelocity.x <= 0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }   
+    }
+}
---
+++ c# uptodate/movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // value fyrir movement
+    public float MovementSpeed = 1;
+    public float JumpForce = 1;
+
+    private Rigidbody2D _rigibody;
+
+
+
+    private void Start()
+    {
+        _rigibody = GetComponent<Rigidbody2D>();
+    }
+    //kóði fyrir player movement 
+    private void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+        //lætur spilara snúast við til að vísa í þá átt sem hann fer
+        if (!Mathf.Approximately(0, movement))
+            transform.rotation = movement > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+        //lætur spilarann hoppa ef space er ítt á
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigibody.velocity.y) < 0.001f)
+        {
+            _rigibody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+
+
+    }
+}
